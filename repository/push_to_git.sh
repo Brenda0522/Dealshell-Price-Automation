7b4b4668386ea95c515cbf66fb1351a1ef980bbb
Quang Pham <quangpham@Quangs-MacBook-Air.local>
2023-09-01 14:51:07 +0100

    Automated commit on 2023-09-01 14:51:07

--- repository/push_to_git.sh
+++ repository/push_to_git.sh
@@ -6,6 +6,7 @@ cd ~/Documents/my_project_aug_2023/project_code/main_project
 # Add and commit changes
 git add .
 git commit -m "Automated commit on $(date +'%Y-%m-%d %H:%M:%S')"
-
+git config --global user.name "binamite"
+git config --global user.email pham29239@gmail.com
 # Push to the remote repository
-git push origin main # Replace 'main' with your branch name if different
+git push origin 'https://github.com/binamite/price_comparison_website' # Replace 'main' with your branch name if different
