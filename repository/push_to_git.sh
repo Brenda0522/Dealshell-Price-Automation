337acdd5116e7ecc82ae9d9a250f38e31384c718
binamite <pham29239@gmail.com>
2023-09-01 16:02:26 +0100

    Automated commit on 2023-09-01 16:02:26

--- repository/push_to_git.sh
+++ repository/push_to_git.sh
@@ -12,4 +12,4 @@ git config --global user.email pham29239@gmail.com
 # # Set the remote URL to the price_comparison_website repository
 # git remote set-url origin 'https://github.com/binamite/price_comparison_website.git'
 # Push to the remote repository (main branch)
-git push 'git@github.com:binamite/price_comparison_website.git' main
+git push 'https://github.com/binamite/price_comparison_website.git' main
