689a155279662b09234f4ca2f05e3fba8486b003
binamite <pham29239@gmail.com>
2023-09-01 16:54:20 +0100

    Automated commit on 2023-09-01 16:54:20

--- repository/push_to_git.sh
+++ repository/push_to_git.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # Define the path to your project directory
-cd /Users/quangpham/Documents/my_project_aug_2023/project_code/main_project
+project_dir="/Users/quangpham/Documents/my_project_aug_2023/project_code/main_project"
 
 # Change to the project directory
+cd "$project_dir" || exit
 
 # Add and commit changes
 git add .
@@ -13,6 +14,9 @@ git config --global user.email pham29239@gmail.com
 
 # Set the remote URL to the price_comparison_website repository
 git remote set-url origin 'https://github.com/binamite/price_comparison_website.git'
-# Push to the remote repository (project branch)
+
+# Pull the latest changes from the remote "project" branch
 git pull origin project
-git push -f origin project
+
+# Push your changes to the remote "project" branch
+git push origin project
