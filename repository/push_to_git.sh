993d933097fc13cd64542465805e412142f4f375
binamite <pham29239@gmail.com>
2023-09-01 16:20:09 +0100

    Automated commit on 2023-09-01 16:20:09

--- repository/push_to_git.sh
+++ repository/push_to_git.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# Change to your project directory
-cd ~/Documents/my_project_aug_2023/project_code/main_project
+# Define the path to your project directory
+project_dir="/Users/quangpham/Documents/my_project_aug_2023/project_code/main_project"
+
+# Change to the project directory
+cd "$project_dir" || exit
 
 # Add and commit changes
 git add .
@@ -9,7 +12,7 @@ git commit -m "Automated commit on $(date +'%Y-%m-%d %H:%M:%S')"
 git config --global user.name "binamite"
 git config --global user.email pham29239@gmail.com
 
-# # Set the remote URL to the price_comparison_website repository
-# git remote set-url origin 'https://github.com/binamite/price_comparison_website.git'
+# Set the remote URL to the price_comparison_website repository
+git remote set-url origin 'https://github.com/binamite/price_comparison_website.git'
 # Push to the remote repository (main branch)
-git push 'https://github.com/binamite/price_comparison_website.git' main
+git push origin main
