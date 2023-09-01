8a2483dfd1a14ca5d97db9af9cf8c380f3ce36fc
Quang Pham <quangpham@Quangs-MacBook-Air.local>
2023-09-01 14:36:26 +0100

    Automated commit on 2023-09-01 14:36:26

---
+++ repository/push_to_git.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Change to your project directory
+cd /Users/quangpham/Documents/my_project_aug_2023/main_project
+
+# Add and commit changes
+git add .
+git commit -m "Automated commit on $(date +'%Y-%m-%d %H:%M:%S')"
+
+# Push to the remote repository
+git push origin price_comparison_website # Replace 'main' with your branch name if different
