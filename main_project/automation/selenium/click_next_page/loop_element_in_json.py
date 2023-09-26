01b7cf1633136463c32a7f0b55ea176ddfe7f397
binamite <pham29239@gmail.com>
2023-09-27 00:28:22 +0100

    Automated commit on 2023-09-27 00:28:22

--- main_project/automation/selenium/click_next_page/loop_element_in_json.py
+++ main_project/automation/selenium/click_next_page/loop_element_in_json.py
@@ -4,7 +4,6 @@ class Next_Page_Clicking:
     def __init__(self, web_url,next_page_path):
         self.web_url = web_url
         self.next_page_path = next_page_path
-
     @classmethod
     def get_dynamic_website(cls):
         with open("/Users/quangpham/Documents/my_project_aug_2023/project/main_project/main_project/list_data.json", 'r') as json_file:
---
+++ main_project/automation/selenium/click_next_page/test.py
@@ -0,0 +1,2 @@
+# from next_page_click import Page_clicker
+# Page_clicker().automate_clicking_through_pages()
